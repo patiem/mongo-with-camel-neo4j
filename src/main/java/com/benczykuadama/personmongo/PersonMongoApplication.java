d680c159560d93edb7dd0f043952a2ddd15d39cc
patiem <pp.mikulska@gmail.com>
2018-05-15 10:43:26 +0200

    Works for camel 2.21

--- src/main/java/com/benczykuadama/personmongo/PersonMongoApplication.java
+++ src/main/java/com/benczykuadama/personmongo/PersonMongoApplication.java
@@ -5,12 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ImportResource;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-//@ImportResource({"classpath:spring/camel-context.xml"})
 public class PersonMongoApplication {
 
     public static void main(String[] args) {
