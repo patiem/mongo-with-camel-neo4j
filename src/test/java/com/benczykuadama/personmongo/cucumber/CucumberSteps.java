a2c118f7a8c8b1f11f1ff8b6af881bd009c8b704
patiem <pp.mikulska@gmail.com>
2018-06-04 15:59:58 +0200

    Add half of new test

--- src/test/java/com/benczykuadama/personmongo/cucumber/CucumberSteps.java
+++ src/test/java/com/benczykuadama/personmongo/cucumber/CucumberSteps.java
@@ -2,16 +2,24 @@ package com.benczykuadama.personmongo.cucumber;
 
 import com.benczykuadama.personmongo.TestConf;
 import com.benczykuadama.personmongo.model.User;
+import com.benczykuadama.personmongo.repository.FriendRepository;
+import com.benczykuadama.personmongo.repository.UserRepository;
+import com.benczykuadama.personmongo.service.FriendService;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import io.cucumber.datatable.DataTable;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
@@ -25,10 +33,20 @@ public class CucumberSteps {
     private Integer port;
 
     private User userJohn;
+    private User userLucy;
 
     private Response response;
     private ValidatableResponse json;
     private RequestSpecification request;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private FriendRepository neoRepository;
+
+    @Autowired
+    private FriendService friendService;
 
     @Before
     public void setup() {
@@ -61,6 +79,36 @@ public class CucumberSteps {
         assertEquals(stat, response.getStatusCode());
     }
 
+    @Given("Two users")
+    public void two_users(List<Map<String,String>> dataTable) {
+        userJohn = userFromMap(dataTable.get(0));
+        userLucy = userFromMap(dataTable.get(1));
 
+        userRepository.save(userJohn);
+        userRepository.save(userLucy);
+
+        friendService.save(userJohn);
+        friendService.save(userLucy);
+
+
+    }
+
+    @When("^(\\w+) invites (\\w+) to become friends")
+    public void john_invites_lucy_to_become_friends(String user, String friend) {
+
+         String url = String.format("%s/invite/%s", user, friend);
+        response = given().port(port).contentType("application/json").body(userJohn)
+                .when().post(url);
+    }
+
+    @Then("lucy has invitation from john")
+    public void lucy_has_invitation_from_john() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    private User userFromMap(Map<String, String> userMap) {
+        return new User(userMap.get("name"), userMap.get("city"), userMap.get("date"));
+    }
 
 }
--- src/test/resources/cucumber/postuser.feature
+++ src/test/resources/cucumber/postuser.feature
@@ -5,3 +5,14 @@ Feature: System allows registering a new user
     When POST is performed on users
     Then User is added to system registry and returned with MongoID
     And StatusCode is 200
+
+
+  Scenario:  User can invite some Friend
+    Given Two users
+      | name | city| date       |
+      | john | Aaa | 17-05-1979 |
+      | lucy | Bbb | 18-06-1988 |
+
+    When john invites lucy to become friends
+    Then lucy has invitation from john
+    And StatusCode is 200
